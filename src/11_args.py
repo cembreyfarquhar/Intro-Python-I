a1fcead81de7fda93f6b380cf7a47ce4cca944d1
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-05-28 22:05:13 -0500

    Stuck on line 37. Moving on for now

--- src/11_args.py
+++ src/11_args.py
@@ -6,12 +6,25 @@
 
 # YOUR CODE HERE
 
+
+def f1(x, y):
+    return x + y
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*args):
+    x = 0
+    for arg in args:
+        x += arg
+    return x
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
